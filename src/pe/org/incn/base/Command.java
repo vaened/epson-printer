e6e2111460be8c174a78ed934eda4380163dd2a4
enea dhack <enea.so@live.com>
2017-07-27 17:09:20 -0500

    Add more commands

--- src/pe/org/incn/base/Command.java
+++ src/pe/org/incn/base/Command.java
@@ -8,7 +8,36 @@ package pe.org.incn.base;
 public class Command {
     
     /**
-     * Break line command
+     * Break line.
      */
     public static final String BREAK = "uF";
+    
+    /**
+     * Inser black line.
+     */
+    public static final String BLANK_LINE = "\n";
+    
+    /**
+     * Bold text.
+     */
+    public static final String BOLD = "\u001b|bC";
+    
+    /**
+     * Center text.
+     */
+    public static final String CENTER = "\u001b|cA";
+    
+    public static final String HEX = "\u001b|";
+    
+    
+    /**
+     * Convert a text to bold.
+     *
+     * @param commands
+     * @return
+     */
+    public static String prepare(String[] commands) {
+        String join = String.join("", commands);
+        return Command.HEX.concat(join);
+    }
 }
