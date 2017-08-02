faab7f26fdf45ded1d2001216d524a96fdd1b6ba
enea dhack <enea.so@live.com>
2017-08-02 13:53:50 -0500

    Add writer methods

--- src/pe/org/incn/base/SimpleWriter.java
+++ src/pe/org/incn/base/SimpleWriter.java
@@ -3,6 +3,7 @@ package pe.org.incn.base;
 import jpos.JposException;
 import jpos.POSPrinterConst;
 import pe.org.incn.support.CollectionUtils;
+import pe.org.incn.support.Helpers;
 
 /**
  * SimpleWriter
@@ -23,17 +24,12 @@ public class SimpleWriter implements WriterContract {
     }
 
     /**
-     * Write text in the document.
-     *
-     * @param text
-     * @param commands
-     * @param overrideCommands
-     * @return
-     * @throws jpos.JposException
+     * {@inheritDoc }
      */
-    public WriterContract writer(String text, String[] commands, String[] overrideCommands) throws JposException {
-        String command = Command.prepare(CollectionUtils.join(commands, overrideCommands));
-        this.printer.printNormal(POSPrinterConst.PTR_S_RECEIPT, command.concat(text));
+    @Override
+    public WriterContract write(String text, String[] commands, String... overrideCommands) throws JposException {
+        text = Command.prepare(text, CollectionUtils.join(commands, overrideCommands));
+        this.printer.printNormal(POSPrinterConst.PTR_S_RECEIPT, text);
         return this;
     }
 
@@ -42,7 +38,7 @@ public class SimpleWriter implements WriterContract {
      */
     @Override
     public WriterContract writeBoldLine(String text, String... commands) throws JposException {
-        return this.writer(text, commands, new String[]{
+        return this.write(text, commands, new String[]{
             Command.BOLD,
             Command.BLANK_LINE
         });
@@ -53,8 +49,8 @@ public class SimpleWriter implements WriterContract {
      */
     @Override
     public WriterContract writeLine(String text, String... commands) throws JposException {
-        return this.writer(text, commands, new String[]{
-            Command.BLANK_LINE            
+        return this.write(text, commands, new String[]{
+            Command.BLANK_LINE
         });
     }
 
@@ -65,7 +61,7 @@ public class SimpleWriter implements WriterContract {
     public WriterContract centerWords(String text, String... commands) throws JposException {
         this.centering(text, commands, new String[]{
             Command.CENTER,
-            Command.BLANK_LINE            
+            Command.BLANK_LINE
         });
 
         return this;
@@ -86,6 +82,46 @@ public class SimpleWriter implements WriterContract {
     }
 
     /**
+     * {@inheritDoc }
+     */
+    @Override
+    public WriterContract groupOneLineWords(String label, String text, String... commands) throws JposException {
+        return commonGroup(label, text, true, commands);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public WriterContract groupWords(String label, String text, String... commands) throws JposException {
+        return commonGroup(label, text, false, commands);
+    }
+
+    /**
+     * Group text and label
+     *
+     * @param label
+     * @param text
+     * @param oneline
+     * @param commands
+     * @return
+     * @throws JposException
+     */
+    protected WriterContract commonGroup(String label, String text, boolean oneline, String... commands) throws JposException {
+        label += ":";
+
+        this.write(label, oneline ? new String[]{
+            Command.BOLD
+        } : new String[]{
+            Command.BOLD,
+            Command.BLANK_LINE
+        });
+
+        text = " " + text;
+        return this.writeLine(text, commands);
+    }
+
+    /**
      * Center words in the document.
      *
      * @param text
@@ -97,8 +133,12 @@ public class SimpleWriter implements WriterContract {
         int length = text.length(),
                 max_width = this.getMaxWith();
 
+        if (Helpers.existInStringArray(commands, Command.LARGE_SIZE) || Helpers.existInStringArray(commands, Command.MEDIUM_SIZE)) {
+            max_width /= 2;
+        }
+
         if (length <= max_width) {
-            this.writer(text, commands, overrideCommands);
+            this.write(text, commands, overrideCommands);
             return;
         }
 
@@ -110,18 +150,17 @@ public class SimpleWriter implements WriterContract {
             prev = prev.substring(0, found);
         }
 
-        this.writer(prev, commands, overrideCommands);
+        this.write(prev, commands, overrideCommands);
         centering(text.substring(found, length), commands, overrideCommands);
     }
-    
+
     /**
      * Returns the maximum width of the canvas.
      *
      * @return
      * @throws JposException
      */
-    protected int getMaxWith() throws JposException
-    {
+    protected int getMaxWith() throws JposException {
         return this.printer.getRecLineChars();
     }
 }
--- src/pe/org/incn/base/WriterContract.java
+++ src/pe/org/incn/base/WriterContract.java
@@ -10,6 +10,17 @@ import jpos.JposException;
 public interface WriterContract {
 
     /**
+     * Write text in the document.
+     *
+     * @param text
+     * @param commands
+     * @param overrideCommands
+     * @return
+     * @throws jpos.JposException
+     */
+    public WriterContract write(String text, String[] commands, String... overrideCommands) throws JposException;
+
+    /**
      * Write bold text in the document.
      *
      * @param text
@@ -48,4 +59,27 @@ public interface WriterContract {
      * @throws JposException
      */
     public WriterContract centerWords(String text, String... commands) throws JposException;
+
+    /**
+     * Write a block of text in a single line, separating the label from the
+     * text with 2 points.
+     *
+     * @param label
+     * @param text
+     * @param commands
+     * @return
+     * @throws JposException
+     */
+    public WriterContract groupOneLineWords(String label, String text, String... commands) throws JposException;
+
+    /**
+     * Write a block of text on a separate line for the label and text.
+     *
+     * @param label
+     * @param text
+     * @param commands
+     * @return
+     * @throws JposException
+     */
+    public WriterContract groupWords(String label, String text, String... commands) throws JposException;
 }
