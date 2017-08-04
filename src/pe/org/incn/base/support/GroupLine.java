6629f2c91c79ec44246f673c0c27c82b561e5a37
enea dhack <enea.so@live.com>
2017-08-04 18:20:04 -0500

    Add group builder

--- src/pe/org/incn/base/support/GroupLine.java
+++ src/pe/org/incn/base/support/GroupLine.java
@@ -1,5 +1,16 @@
 package pe.org.incn.base.support;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import pe.org.incn.base.support.models.BaseWordsGroup;
+import pe.org.incn.main.Configuration;
+import pe.org.incn.support.Helpers;
+
 /**
  * GroupLine
  *
@@ -7,40 +18,108 @@ package pe.org.incn.base.support;
  */
 public class GroupLine {
 
-    private final String group;
-    private final boolean independentLine;
+    private final Stream<BaseWordsGroup> group;
 
     /**
+     * GroupLine constructor.
      *
      * @param group
      */
-    public GroupLine(String group) {
+    public GroupLine(Stream<BaseWordsGroup> group) {
         this.group = group;
-        this.independentLine = false;
     }
 
     /**
      *
-     * @param label
-     * @param text
+     * @return
      */
-    public GroupLine(String label, String text) {
-        this.group = label + text;
-        this.independentLine = true;
+    public String make() {
+        Helpers.printRules();
+        List<String> lines = new ArrayList<>();
+        List<BaseWordsGroup> groups = this.group.collect(Collectors.toList());
+
+        Collections.sort(groups, (BaseWordsGroup o1, BaseWordsGroup o2) -> {
+            return Boolean.compare(!o1.allWidth(), !o2.allWidth());
+        });
+
+        lines.addAll(this.buildLines(Helpers.divideList(groups, 2)));
+
+        return String.join("", lines);
     }
 
     /**
-     * @return the group
+     *
+     * @param groupList
+     * @return
      */
-    public String getGroup() {
-        return group;
+    protected List<String> buildLines(List<List<BaseWordsGroup>> groupList) {
+        List<String> lines = new ArrayList<>();
+
+        groupList.forEach(groups -> {
+            BaseWordsGroup group = groups.get(0);
+            String firstLine = this.buildFirstLine(group);
+            String lastLine = this.buildLastLine(group);
+
+            if (groups.size() == 2) {
+                group = groups.get(1);
+                firstLine += this.buildFirstLine(group);
+                lastLine += this.buildLastLine(group);
+            }
+
+            lines.add(firstLine);
+
+            if (!lastLine.trim().equals("")) {
+                lines.add(lastLine);
+            }
+
+        });
+
+        return lines;
     }
 
     /**
-     * @return the independentLine
+     * Built the first line in the document.
+     *
+     * @param group
+     * @return
      */
-    public boolean isIndependentLine() {
-        return independentLine;
+    protected String buildFirstLine(BaseWordsGroup group) {
+        if (this.isFullWidth(group)) {
+            return group.getBuiltLabel() + group.getBuiltText();
+        }
+
+        return group.getBuiltLabel();
     }
     
+    /**
+     * Built the second line in the document.
+     *
+     * @param group
+     * @return
+     */
+    protected String buildLastLine(BaseWordsGroup group) {
+        if (this.isFullWidth(group)) {
+            return Helpers.rightAutocomplete("", this.getMiddleWidth());
+        }
+
+        return group.getBuiltText();
+    }
+
+    /**
+     * returns true is occupied full line
+     *
+     * @param group
+     * @return
+     */
+    protected boolean isFullWidth(BaseWordsGroup group) {
+        return group.allWidth() || group.getTotalLengthWithSpaces() <= this.getMiddleWidth();
+    }
+
+    /**
+     *
+     * @return
+     */
+    protected int getMiddleWidth() {
+        return Configuration.getCanvasMaxWidth() / 2;
+    }
 }
