6b8864816e89d04689ee9c95282ba315187df9a7
enea dhack <enea.so@live.com>
2017-09-28 13:30:16 -0500

    Add methods for transactions

--- src/pe/org/incn/base/EpsonPrintable.java
+++ src/pe/org/incn/base/EpsonPrintable.java
@@ -1,11 +1,25 @@
 package pe.org.incn.base;
 
+import jpos.JposException;
 import jpos.POSPrinterControl114;
 
 /**
  *
  * @author efloresp
  */
-public interface EpsonPrintable extends POSPrinterControl114{
-    
+public interface EpsonPrintable extends POSPrinterControl114 {
+
+    /**
+     * Start the print transanction.
+     *
+     * @throws JposException
+     */
+    public void beginTransactionPrint() throws JposException;
+
+    /**
+     * End the print transanction.
+     *
+     * @throws JposException
+     */
+    public void endTransactionPrint() throws JposException;
 }
--- src/pe/org/incn/base/Printer.java
+++ src/pe/org/incn/base/Printer.java
@@ -1,12 +1,23 @@
 package pe.org.incn.base;
 
+import jpos.JposException;
 import jpos.POSPrinter;
+import jpos.POSPrinterConst;
 
 /**
  * Printer
  *
  * @author enea <enea.so@live.com>
  */
-public class Printer extends POSPrinter implements EpsonPrintable{
+public class Printer extends POSPrinter implements EpsonPrintable {
 
+    @Override
+    public void beginTransactionPrint() throws JposException {
+        this.transactionPrint(POSPrinterConst.PTR_S_RECEIPT, POSPrinterConst.PTR_TP_TRANSACTION);
+    }
+
+    @Override
+    public void endTransactionPrint() throws JposException {
+        this.transactionPrint(POSPrinterConst.PTR_S_RECEIPT, POSPrinterConst.PTR_TP_NORMAL);
+    }
 }
