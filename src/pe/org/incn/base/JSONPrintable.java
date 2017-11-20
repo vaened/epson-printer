bad2767aae575d0244c452b3914123e00fdc0505
enea dhack <enea.so@live.com>
2017-11-20 16:17:21 -0500

    Refactor JSONPrintable

--- src/pe/org/incn/base/JSONPrintable.java
+++ src/pe/org/incn/base/JSONPrintable.java
@@ -1,7 +1,7 @@
 package pe.org.incn.base;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import pe.org.incn.json.JSONArray;
+import pe.org.incn.json.JSONObject;
 
 /**
  * JSONPrintable
@@ -10,79 +10,30 @@ import org.json.JSONObject;
  */
 public abstract class JSONPrintable extends Printable {
 
-    protected JSONObject json;
+    protected JSONObject object;
 
-    public JSONPrintable(JSONObject json, EpsonPrintable printer) {
-        super(printer);
-        this.json = json;
+    public JSONPrintable(JSONObject object, EpsonPrintable printable) {
+        super(printable);
+        this.object = object;
     }
-
-    protected String value(JSONObject json, String key, String defaultValue) {
-        try {
-            if (!json.has(key)) {
-                return defaultValue;
-            }
-
-            Object o = json.get(key);
-
-            if (o == null) {
-                return defaultValue;
-            }
-
-            if (o instanceof Integer) {
-                return o.toString();
-            }
-
-            if (o instanceof Double) {
-                return o.toString();
-            }
-
-            return (String) o;
-        } catch (Exception ex) {
-            return defaultValue;
-        }
-    }
-
-    protected String value(JSONObject json, String key) {
-        return this.value(json, key, "");
-    }
-
+    
     public String json(String key) {
-        return this.json(key, "");
+        return this.object.json(key);
     }
 
-    protected boolean hasConfiguration() {
-        return this.json.has("config");
+    public String json(String key, String defaultValue) {
+        return this.object.json(key, defaultValue);
+    }
+
+    public JSONArray getJSONArray(String key) {
+        return this.object.getJSONArray(key);
     }
 
     public String config(String key) {
-        try {
-            return this.value(json.getJSONObject("config"), key, "");
-        } catch (JSONException e) {
-            return "";
-        }
+        return this.getConfig().json(key);
     }
 
-    public String config(String key, String defaultValue) {
-        try {
-            return this.value(json.getJSONObject("config"), key, defaultValue);
-        } catch (JSONException e) {
-            return defaultValue;
-        }
+    public JSONObject getConfig() {
+        return this.object.getJSONObjec("config");
     }
-
-    /**
-     *
-     * @param key
-     * @param defaultValue
-     * @return
-     */
-    public String json(String key, String defaultValue) {
-        if (!this.json.has(key)) {
-            return defaultValue;
-        }
-
-        return this.value(this.json, key, defaultValue);
-    }
-
 }
